f77134d05cafc3bce555efcc25347091f7b2f7e5
Gourav Panseja <gouravpanseja25@gmail.com>
2024-08-23 00:54:19 +0530

    firebase google auth bug-3

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,15 +34,17 @@ exports.firebaseSignup = asyncHandler(async(req, res, next)=>{
             throw new ApiError(404, "google user not found");
         }
 
-        const googleData = googleUser?.providerData?.filter((data)=> data.providerId === "google.com")[0];
+        if(!googleUser.email_verified){
+            throw new ApiError(400, "email verification failed");
+        }
+        const email = googleUser.email;
 
-        const {displayName} = googleData;
+        const {name} = googleUser;
 
-        firstName = displayName.split(" ")[0];
-        lastName = displayName.split(" ")[1];
+        firstName = name.split(" ")[0];
+        lastName = name.split(" ")[1];
 
-        email = googleData.email;
-        avatarUrl = googleData.photoURL;
+        avatarUrl = googleUser.picture;
     }
             
     // check if user already registered
@@ -57,7 +59,7 @@ exports.firebaseSignup = asyncHandler(async(req, res, next)=>{
         firstName,
         lastName,
         password,
-        photoUrl,
+        avatarUrl,
         provider,
     }).select("-password -previousIternaries");
 
